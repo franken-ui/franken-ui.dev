ae2805e51f241b0b342ed1f67fc18bba853f44da
Sveltecult <sveltecult@proton.me>
2024-08-05 08:36:53 +0800

    Install Franken UI: Dos

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import franken from "franken-ui/plugin-vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      tailwindcss(),
+      franken({
+        preflight: false,
+        layer: true,
+        layerExceptions: ["chart"],
+      }),
+    ],
+  },
+});
